95ab8c9e7f3dd7237287830564820d1ee7533644
“yashchohtel” <yashchohtel@gmail.com>
2024-02-10 19:47:58 +0530

    file created

---
+++ JAVASCRIPT/01. Introduction.js
@@ -0,0 +1,50 @@
+/*
+
+What is Javascript?
+
+Javascript is a text-based programmin language used both one the client side and srevr side that allows you to make web pages interactive.
+Javascript improves the user experience of the webpage by converting it from a static page into an interactive one. Or javascript adds behavior to web page.
+
+Javascript is a light-weight object-oriented programming language which is used by several websites for scriptig the webpages. It is an interpreted, full-fleged programming language that enables dynamic interactivty when applied to an html document.
+
+Why Javascript was born?
+- To create interactive websites.
+- Client side validation
+- Popup
+- Events on click and many many more things
+
+Where is JS now?
+
+- Website client side (js, jquery, rjs....etc)
+- Website server side (node js, express js)
+- Mobile development (react natic, phone gap, ionic...)
+- Software Development (electronjs Ex- Vscode)
+
+What is ES6?
+ES6 stands for ECMAScript 6. ECMAScript was created to standardize JavaScript, and ES6 is the 6th version of ECMAScript, it was published in 2015, and is also known as ECMAScript 2015.
+
+ECMAScript provides the rules, details and guidelines that a scipting language must observe to be considered ECMASCript compliant.
+
+Javascript is also a general purpose scripting language that conforms to the ecmascript specification. 
+
+By reading the ECMAScript specification, you learn how to screate a scripting language. 
+By reading te Javscript documentation you learn how to sue a scripting language. 
+Javascript implements the ecmascript specification as described in ECMA-262 
+
+Whatis es6,es7,es8, and so on..?
+
+ECMASCript 6
+It is the sixth edition of the ECMA-262 standard and features major changes and improvements to the ecmascript specification. It is also known as ES6 , es2015 and ecmascript 2015. ES6 and es2015 are two different names for the same things.
+
+
+USAGE OF JAVASCRIPT
+
+FRONTEND - we can build solid frontend with the help of javascript frontend library like react, angular, veujs etc.
+BACKEND - with the help of node js and express js we can build backend also.
+MOBILE APP DEVELOPMENT - with the help of javascript react native library we can build native mobile application for android and ios.
+DESKTOP APP DEVLOPMENT - with the help of electron js we can build desktop applicaiton for windows linux and macos.
+GAME DEVELOPMENT - we can also build game with the help of canvas in javascript.
+MACHINE LEARNING - With the help of tencerflow js liberary we can do machine learning also.
+BROWSER EXTENSION - js can also used for creating browser extersion.
+
+*/  
