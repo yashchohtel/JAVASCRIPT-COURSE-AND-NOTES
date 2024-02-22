8c16a4d0bc89d5bcd71395a9676be776e6371ef0
“yashchohtel” <yashchohtel@gmail.com>
2024-02-22 19:39:44 +0530

    done

--- JAVASCRIPT/01. Introduction.js
+++ JAVASCRIPT/01. Introduction.js
@@ -2,7 +2,7 @@
 
 What is Javascript?
 
-Javascript is a text-based programmin language used both one the client side and srevr side that allows you to make web pages interactive.
+Javascript is a text-based programmin language used both on the client side and srevr side that allows you to make web pages interactive.
 Javascript improves the user experience of the webpage by converting it from a static page into an interactive one. Or javascript adds behavior to web page.
 
 Javascript is a light-weight object-oriented programming language which is used by several websites for scriptig the webpages. It is an interpreted, full-fleged programming language that enables dynamic interactivty when applied to an html document.
@@ -17,7 +17,7 @@ Where is JS now?
 
 - Website client side (js, jquery, rjs....etc)
 - Website server side (node js, express js)
-- Mobile development (react natic, phone gap, ionic...)
+- Mobile development (react natic, phone aap, ionic...)
 - Software Development (electronjs Ex- Vscode)
 
 What is ES6?
@@ -28,7 +28,7 @@ ECMAScript provides the rules, details and guidelines that a scipting language m
 Javascript is also a general purpose scripting language that conforms to the ecmascript specification. 
 
 By reading the ECMAScript specification, you learn how to screate a scripting language. 
-By reading te Javscript documentation you learn how to sue a scripting language. 
+By reading the Javscript documentation you learn how to sue a scripting language. 
 Javascript implements the ecmascript specification as described in ECMA-262 
 
 Whatis es6,es7,es8, and so on..?
@@ -47,6 +47,48 @@ GAME DEVELOPMENT - we can also build game with the help of canvas in javascript.
 MACHINE LEARNING - With the help of tencerflow js liberary we can do machine learning also.
 BROWSER EXTENSION - js can also used for creating browser extersion.
 
-*/  
+HISTORY OF JAVASCRIPT :-
 
+In 1995 -  "Brendan Eich" a computer scientist in Netscape created a javascript language in just 10 days at that time javascript firstly known as MOCHA then it renamed to LiveScript and then Javascript
 
+Switching from Livescript to javascript was a smart move to make it sound cooler and piggyback on Java's fame while also cozying up to sun Microsystems.
+
+====================================================================
+
+There are three way to write javascript :-
+
+Inline Javascript - Inline JavaScript is JavaScript code that is typed directly into an event property in the Visual Designer rather than defined in an external .js file. Inline JavaScript is most appropriate when you only need to utilize one or just a few lines of code to perform the desired action.
+
+for example :- <button onclick="alert('hello')">click me</button>
+
+Internal Jvascript :- Internal JavaScript: JavaScript can be added directly to the HTML file by writing the code inside the <script> tag . We can place the <script> tag  either inside <head> or the <body> tag  according to the need.
+
+for example :-
+<script>
+    alert("Hello World...")
+</script>
+
+External Javascript :- External JavaScript is when the JavaScript Code(script) is written in another file having an extension . js and then we link this file inside the <head> or<body> tag of our HTML file in which the code is to be added.
+
+for example :-  <script src="script.js"></script>
+
+====================================================================
+
+Rules and naming convention for writing syntax in javascript 
+
+1. Javascript is case sensetive.
+2. While declareing variable we can use letters lowercase (a-z), uppercase(A-Z), Numbers (0-9), Special Symbol only dollar sign and underscore ($ and _)
+3. Variables cannot contain whitespace characters (tabs and sapce).
+4. A variable cannot begin with a numbers.
+5. We cannot use reserved words in a variable name.
+6. It is prefered to used camel casing, Means first word start with a lowercase letter, rest all words with capital letter
+7. Booleand name should be prefixed with something like (isUserLogin, isAdmin, hasRole etc..)
+8. In functions names it is good practice to prefix it with nouns and verbs like [ getUserName(), setDate(), getAgeData() etc...].
+9. In constants it is good practive to write names in all uppercase if one word  like [ PI 3.14 ], and snake upper caseing if more then two words like [UPPER_SNAKE_CASE, ORG_NAME etc...] .
+10. In classes name if is preffer to use pascalcase like ( ConstructorName{}, ClassName{}  etc.... ).
+
+====================================================================
+
+Console in Javascript :- The Console can be used to log information as part of the JavaScript development process, as well as allow you to interact with a web page by carrying out JavaScript expressions within the page's context. Essentially, the Console provides you with the ability to write, manage, and monitor JavaScript on demand. It also shows the error of code. 
+
+*/
