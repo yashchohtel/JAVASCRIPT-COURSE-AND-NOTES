29f4d9b46e95321e0c6624e8c42e797de05ae2ab
“yashchohtel” <yashchohtel@gmail.com>
2024-02-23 22:28:05 +0530

    file modified

--- JAVASCRIPT/02. Value and variable.js
+++ JAVASCRIPT/02. Value and variable.js
@@ -1,6 +1,48 @@
 /*
 
+VALUES AND VARIABLES :- 
 
+In JavaScript, values and variables are fundamental concepts that form the basis of programming.
+Values: A value is a piece of information that a program can work with. It can be a number, text, true/false, or more complex data.
+Variables: A variable is a container that holds a value. It has a name and can be used to store and manipulate data in a program.
+
+Meaning of Variables in general :-
+
+Definition. A variable is any characteristic, number, or quantity that can be measured or counted. A variable may also be called a data item. Age, sex, business income and expenses, country of birth, capital expenditure, class grades, eye colour and vehicle type are examples of variables.
+
+A variable means, a things which can vary or which can be changed.
+
+Meaning of a VARIABLE in programming :-
+
+The name of memory location is called variable. in simple words we can say that variable is like a container  which is used to store data. Variable are changeable, we can change the value of variable during the execution of a programm and variable can be used many types.
+
+Rules and best practice to declare a variable :-
+
+. Variable names must start with a letter, an underscore (_) or a dollar sign ($).
+. Variable names canot contain spaces. 
+. By convention, javascript variable names are written in camelCase.
+. Variable cannot be the same as reserved keywords such as if or const.
+. Variable names are case sensitive.
+. VAriable names can be as long as you need.
+
+
+
+VARIABLE IN JAVASCRIPT :- Like many other programming languages, JavaScript has variables. Variables can be thought of as named containers. You can place data into these containers and then refer to the data simply by naming the container.
+
+Before you use a variable in a JavaScript program, you must declare it. Variables are declared with the var keyword as follows.
+
+var myName = "YASH CHOHTEL";
+keyword | variable name | assignment operator | variable value
+
+"var" is the keyword that tells JavaScript you’re declaring a variable.
+"myName" is the name of that variable.
+"=" is the operator that tells JavaScript a value is coming up next.
+"YASH CHOHTEL" is the vaule of variable.
+
+Here writin var keyword and name is vaiable is called declearing a variable and assigning a value to it is called initiallizing a variable.
+
+meaning of declearing a variable - Declaration of a variable in a computer programming language is a statement used to specify the variable name and its data type. Declaration tells the compiler about the existence of an entity in the program and its location. 
+meaning of assigning a variable - Assigning a value is a process that defines the value of a variable
+meaning of initializing a variable -  Initialization is the process of assigning a value to the Variable.
 
 */ 
-
