8cd4adcd0d62cb52d00a008d0c73493a38653fb8
“yashchohtel” <yashchohtel@gmail.com>
2024-03-20 20:21:40 +0530

    file modified

--- JAVASCRIPT/06. Loops.js
+++ JAVASCRIPT/06. Loops.js
@@ -1,5 +1,9 @@
 /*
 
+LOOPS IN JAVASCRIPT :-
+
+Loops in programming are control structures that allow you to execute a block of code repeatedly based on a condition. They are fundamental for automating repetitive tasks and iterating over collections of data.
+
 
 
 */ 
