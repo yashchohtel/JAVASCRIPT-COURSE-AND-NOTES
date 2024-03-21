daff99f8fb7c42d7ad0a2d9a810f70bd9531733e
“yashchohtel” <yashchohtel@gmail.com>
2024-03-21 18:57:41 +0530

    file modified

--- JAVASCRIPT/06. Loops.js
+++ JAVASCRIPT/06. Loops.js
@@ -4,6 +4,41 @@ LOOPS IN JAVASCRIPT :-
 
 Loops in programming are control structures that allow you to execute a block of code repeatedly based on a condition. They are fundamental for automating repetitive tasks and iterating over collections of data.
 
+-------------------------
+
+1. WHILE LOOP :- A while loop in javascript is a control structure that repetedly executes a block of code as long as a specified condition remains true. The loop continnues iterating while the condition is true, and it terminates when the condition becomes false.
+
+
+syntax :-
+while(condition){
+    code to be executed as long as the condition is true
+}
+
+example :-
+
+-------------------------
+
+2. DO WHILE LOOP :- In JavaScript, a do-while loop is a control flow statement that allows you to execute a block of code repeatedly as long as a specified condition evaluates to true. The main difference between a do-while loop and a while loop is that a do-while loop guarantees that the block of code inside the loop will be executed at least once, regardless of whether the condition is initially true or false.
+
+syntax :- 
+do(
+    code to be executed at least once
+) while (condititon);
+
+
+
+
+
+
+
+
+
 
 
 */ 
+
+let counter = 1;
+
+while (counter<10) {
+    console.log(counter);
+}
