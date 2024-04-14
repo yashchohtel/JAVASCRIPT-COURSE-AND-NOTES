47852848b440be3325465e04a97e76fabbb6d3cf
“yashchohtel” <yashchohtel@gmail.com>
2024-04-14 20:18:45 +0530

    file modified

--- JAVASCRIPT/07. Function.js
+++ JAVASCRIPT/07. Function.js
@@ -2,5 +2,52 @@
 
 FUNCTION IN JSVASCRIPT :-
 
+In javascript, a function is a block of resuable code that performs a specific tast or set of tasks. functions are used to organize code into modular and manageable pieces, promote code reuse, and make programs more readable.
+
+
+Syntax :-
+
+function functionName(parameters){
+    code to be executed
+    return reslut: // optional return statement
+}
+
+functionName(); // this is calling the function
+
+---------
+
+example 1:- (say hello world with the helop of fucntion)
+
+function sayHello(){
+    console.log('hello world!');
+}
+
+sayHello(); // output: hello world!
+
+---------
+
+example 2 :- (function with a parameter and argument)
+
+function sayName(myName){
+    console.log("my name is " + myName);
+}
+
+sayName("yash chohtel");
+
+---------
+
+example 3 :- (function with multiple parameter and argument)
+
+function sayFullName(firstName, lastName){
+    console.log("my name is " + firstName + " " + lastName);
+};
+
+sayFullName("yash","chohtel");
+
 
 */ 
+
+
+
+
+
