6cde94c7b8a7c65c58c349bd1b1f6c3e51f2d1e6
“yashchohtel” <yashchohtel@gmail.com>
2024-04-27 22:54:14 +0530

    done

---
+++ JAVASCRIPT/09. Default Parameter (ES6).js
@@ -0,0 +1,18 @@
+/*
+
+DEFAULT PARAMETERS :-
+
+In ECMAScript 6 (ES6). default parameters were introduced, providing a more consise way to handle function parameter defaults. Default parameters allow you to specify deault values for function parameters in te function declaration itself.If a parameter is not provided when the function is called, the default value is used.
+
+Dedfault function parameters allow named parameters to be initialized with deault values if no values or undefined is passed.
+
+example 1:- 
+
+function sum(num1 = 10,num2 = 20){
+    console.log(num1+num2);
+}
+
+sum();
+
+
+*/ 
---
+++ JAVASCRIPT/10. Arrow Function (ES6).js
@@ -0,0 +1,76 @@
+/*
+
+ARROW FUNCTION :-
+
+IN ECMAScript 6 (ES6), arrow function, algo known as fat arrow functions or lambda function, were introduced as a consise way to write annymous functions. Arrow functions provide a shorter syntax compared to traditional function expressions, making them particularly useful for writing compact and expressive code.
+
+Syntax :- 
+
+(parameters) => { function body };
+
+
+example 1:-
+
+let sum = (a,b) => {
+    console.log(`sum of ${a} and ${b} is ${a+b}`);
+}
+
+sum(10,6);
+
+NOTES :- 
+
+1: If the function body consists of a single expression, the braes {} and the return keyword can be ommited.
+
+let sum = (a, b) => `sum of ${a} and ${b} is ${a + b}`;
+console.log(sum(10, 6));
+
+2: If there is only one parameter, the parentheses () around the aramete list can be moitted.
+
+let greet = message => console.log(`${message} and welcome everyone`);
+greet("HELLO");
+
+3: If there are no parameters, use an empty set of parentheses ().
+
+const greet = () => console.log(`welcome everyone`);
+greet();
+
+-----------------------------------------------
+
+example 1 :- Write a js function calculator that take two numbers and an operator as parameters and returns the result of the operation. The function should support addition, subraction, multilication, and  division.
+
+let calculation = (num1, num2, operater) => {
+  let result;
+
+  switch (operater) {
+    case "+":
+      result = num1 + num2;
+      break;
+
+    case "-":
+      result = num1 - num2;
+      break;
+
+    case "*":
+      result = num1 * num2;
+      break;
+
+    case "/":
+      result = num1 / num2;
+  }
+
+  console.log(result);
+};
+
+calculation(4,2,"*")
+
+
+example 2 :- Write a function to reverse a given string without using built-in reverse methods.
+
+*/
+
+
+let isReversed = (str) =>{
+
+}
+
+isReversed("YASH CHOHTEL")
