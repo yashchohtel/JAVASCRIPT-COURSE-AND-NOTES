5af4ec382bf26e470906973784214ef1c35dd9af
“yashchohtel” <yashchohtel@gmail.com>
2024-05-13 23:10:51 +0530

    done

--- JAVASCRIPT/11. Array.js
+++ JAVASCRIPT/11. Array.js
@@ -159,7 +159,7 @@ map - used when you want to create a new aray based on the transformation of eac
 
 ---------------
 
-HOW TO INSERT, ADD, REPLACE AND DELET ELEMENTS IN ARRAY (CRUD) 
+TO INSERT, ADD, REPLACE AND DELET ELEMENTS IN ARRAY FROM START AND END (CRUD) 
 
 1. push() : Method that adds one or moe elements to the end of an array. and return new length of an array.
 
@@ -168,7 +168,6 @@ console.log(fruits);
 let newLength = fruits.push("pear")
 console.log(newLength);
 console.log(fruits);
-
 
 
 2. pop() : method that removes the last element from an array. and returns the poped or deleted element fron an array.
@@ -189,7 +188,6 @@ console.log(newLength);
 console.log(fruits);
 
 
-
 4. shift() : Method that removes the first element from an array. and returns the poped or deleted element fron an array.
 
 let fruits = ["apple", "oranges", "mangoes", "bananas", "grapes", "gwava"];
@@ -198,14 +196,167 @@ let newLength = fruits.shift()
 console.log(newLength);
 console.log(fruits);
 
+QUESTIONS :-
 
-*/
+1. Add Dec at the end of an array.
+let months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov"];
+console.log(months);
+newArr = months.push("dec");
+console.log(newArr);
+console.log(months);
+
+2. Update mar to march
+let months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"];
+
+let indexOfMar = months.indexOf("mar");
+let extractedArr = months.splice(indexOfMar, 1 , "march");
+console.log(extractedArr);
+console.log(months);
+
+3. Delete jun from an array
+let months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"];
+
+let indexOfJun = months.indexOf("jun");
+let extractedArr = months.splice(indexOfJun, 1 );
+console.log(extractedArr);
+console.log(months);
+
+---------------
+
+TO ADD OR REMOVE ELEMENT ANYWHERE IN AN ARRAY :-
+
+5. splice() : splice method of array instances changes the contents of an array by removing or replacing existing elements and / or adding new elements in place
+
+syntax : splice(start, deleteCount, item1, item2, ---------- itemN)
 
 let fruits = ["apple", "oranges", "mangoes", "bananas", "grapes", "gwava"];
 console.log(fruits);
-let newLength = fruits.shift()
-console.log(newLength);
+let newArr = fruits.splice(1, 2, "aam", "santra", "kela");
+console.log(newArr);
 console.log(fruits);
 
+here 1 is the starting point which is index number of elemet shows we are start from the 1 index number element and 2 is the num of element we want delet it means it will delete the 2 element from the 1 index number and return the removed element from an array. and "amm", "santra", "kela" is the inserted element to the array. if we give the 0 value insted of 2 it will not delete an element but it will insert the new element there. and return empty element.
+
+---------------
+
+SEARCHING IN AN ARRAY :-
+
+6. indexOf() : indexOf method returns the first index at which a give elemnt can be found in the array, or -1 if it is notpresent
+
+syntax : indexOf(searchElement, fromIndex)
+
+let fruits = ["apple", "oranges", "mangoes", "bananas", "grapes", "gwava"];
+console.log(fruits);
+
+let newArr = fruits.indexOf("apple",1); // here "apple" is the element we want to search and 1 is the given index number from where we want to start searching if the element found it will return the elements index number or if not found it will return -1.
+console.log(newArr);
 
 
+7. lastIndexOf() : the lastInddexOf() method of array instances returns the last index at which a given element can be found in the array, or -1 if it is not present. The array is searched backwards, starting at fromindex.
+
+const numbers = [1, 2, 3, 4, 1, 2, 3, 4];
+
+const lastIndex = numbers.lastIndexOf(2);
+console.log(lastIndex); // Output: 5
+
+const lastIndexFromIndex = numbers.lastIndexOf(2, 3);
+console.log(lastIndexFromIndex); // Output: 1
+
+The first lastIndexOf(2) call returns 5 because 2 is found at index 5 in the array.
+The second lastIndexOf(2, 3) call returns 1 because it starts searching from index 3 (moving backwards), and the last occurrence of 2 before index 3 is at index 1.
+If the specified item is not found in the array, lastIndexOf() returns -1.
+
+It's important to note that lastIndexOf() performs a strict equality comparison (===) when searching for the specified item. This means that it checks both the value and the data type of the elements in the array. If you want to perform a different type of comparison, you may need to use a custom function with findIndex() or findLastIndex().
+
+
+8. includes() : the includes mtthod checks whether an array includes a certain element returning true or false
+
+syntax : includes(searchElement, fromIndex)
+
+let fruits = ["apple", "oranges", "mangoes", "apple", "bananas", "grapes", "gwava"];
+console.log(fruits);
+
+let newArr = fruits.includes("apple",4); // here "apple" is the element we want to search and 4 is the given index number from where we want to start searching if the element found it will return true or if not found it will return false.
+console.log(newArr);
+
+---------------
+
+FILLTE IN AN ARRAY
+
+9. find() : find method is used to find the first element in an array that satisties a provided testing function. It retuns the first matching element or undefined if no element is found.
+
+let number = [1, 2, 3, 4, 5, 6, 2, 7, 8, 9, 5];
+let newArr = number.find((curElem,index,arr)=>{
+    return curElem > 8;
+});
+console.log(newArr);
+
+10. findIndex() : The findIndex() method of TypeArray instances returns the index of the first element in a typed array that satisfies the provided testing function. If no element satisfy the testing function -1 is returned.
+
+let number = [1, 2, 3, 4, 5, 6, 2, 7, 8, 9, 5];
+let newArr = number.findIndex((curElem,index,arr)=>{
+    return curElem > 8;
+});
+console.log(newArr);
+
+11. filter () : the filter method creates a new aray with all elements that pass the test implemnted by the provided function.
+
+let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let newArr = number.filter((curElem,index,arr)=>{
+    return curElem >= 1;
+});
+console.log(newArr);
+
+QUESTION :-
+
+1. Delete the value 6 from an array.
+
+let value = 6;
+
+let number = [1, 2, 3, 6, 5, 6, 7, 8, 9, 6];
+
+let newArr = number.filter((curElm)=>{
+    return curElm !== value;
+})
+
+console.log(newArr);
+
+2. Given an array of products where each product has a name and a price, wirte a function that used the filter method to return an array containing only the products with a price less than or equal to 500.
+
+
+let product = [
+  {
+    name: "laptop",
+    price: 1200,
+  },
+  {
+    name: "phone",
+    price: 100,
+  },
+  {
+    name: "tablet",
+    price: 300,
+  },
+  {
+    name: "smartwatch",
+    price: 150,
+  },
+];
+
+let newArr = product.filter((curElm)=>{
+    return curElm.price < 500;
+});
+
+console.log(newArr);
+
+3. find unique value
+
+
+
+*/
+
+let number = [1, 2, 3, 6, 5, 6, 7, 8, 9, 6];
+
+let uniqueValue = number.filter((curElem,index,arr)=>{
+    
+})
