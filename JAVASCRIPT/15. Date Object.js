a09ee8ea5d22bb952c3aed5f4ab1baca10ebde43
“yashchohtel” <yashchohtel@gmail.com>
2024-05-24 23:06:29 +0530

    date object started

---
+++ JAVASCRIPT/15. Date Object.js
@@ -0,0 +1,7 @@
+/*
+
+DATE OBJECT IN JAVASCRIPT :-
+
+The Date object in JavaScript is a built-in object that allows you to work with dates and times. It provides methods for creating, manipulating, and formatting dates and times.
+
+*/ 
