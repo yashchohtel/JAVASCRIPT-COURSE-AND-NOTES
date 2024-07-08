f5f88bdce53505f0c1d43c0270610166b3fea754
“yashchohtel” <yashchohtel@gmail.com>
2024-07-08 12:19:21 +0530

    regular expression done

---
+++ 1. JAVASCRIPT/16. Regular Expression.js
@@ -0,0 +1,18 @@
+/*
+
+REGULAR EXPRESSION :-
+
+Regular expression is a sequence of character that define a search pattern. these pattern are widely used for string earcing and manipluation. in validation, regex helps ensure that strings such as email addresses an passwords conform to expted formats.
+
+Basic Syntax
+
+**literals** : ordinary charaters like "a", "1", or 1%
+**metacharacters** : characters with specil meanings, like "." (any characters), '^ (caret)' (start of string), "$" (end of string), etc.
+**character classes**: sets of characters, like '[a-z]' for any lowercase letter.
+**character classes & shorthand**: In addition to specific character sets, regex includes several shrothand character classes that help match common character types:
+
+### digit characters
+
+
+*/ 
+
