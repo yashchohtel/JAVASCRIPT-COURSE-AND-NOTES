c8ab5d447d0a360f38df16081ab3b6b1476bc2f2
“yashchohtel” <yashchohtel@gmail.com>
2024-07-21 11:32:22 +0530

    event propogation done

---
+++ 1. JAVASCRIPT/25. Event propogation.js
@@ -0,0 +1,11 @@
+/*
+
+EVENT PROPOGATION :-
+
+Evnet propogation refers to the process of how events propagate pr travel through the DOM (documnt object model) Hierarchy.
+
+In javascript there are two phases of event propotation:
+- capturing phase 
+- bubbing phase 
+
+*/ 
